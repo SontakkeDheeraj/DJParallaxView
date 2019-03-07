f23e7bcb9fe53b53ad23f631724dc5a5c86b02ed
Dheeraj Sontakke <dheerajsontakke@Dheerajs-MacBook-Air.local>
2019-03-07 20:42:22 +0530

    Initial Commit

---
+++ DJParallaxView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DJParallaxView
+//
+//  Created by Dheeraj Sontakke on 07/03/19.
+//  Copyright © 2019 Dheeraj Sontakke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
